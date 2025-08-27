a3592a15be207ecc3d26b56efb07ec8bef30454b
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-08-27 11:45:02 +0530

    Time: 43 ms (85.48%) | Memory: 13.7 MB (22.35%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        res=[]
+        if len(p)>len(s):
+            return []
+        p_count=[0]*26
+        s_count=[0]*26
+
+        for c in p:
+            p_count[ord(c)-ord('a')]+=1
+        for i in range(len(p)):
+            s_count[ord(s[i])-ord('a')]+=1
+        if s_count==p_count:
+            res.append(0)
+
+
+        for i in range(len(p),len(s)):
+            s_count[ord(s[i])-ord('a')]+=1
+            s_count[ord(s[i-len(p)])-ord('a')]-=1 
+            if s_count==p_count:
+                res.append(i-len(p)+1)
+
+        return res                   
+
+        
