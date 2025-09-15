03f05aca5d5fafe41366b84b1f7c84608a15921f
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-09-15 11:25:40 +0530

    Time: 423 ms (11.82%) | Memory: 29.2 MB (99.09%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.py
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,13 +1,18 @@
 class Solution(object):
+    
     def countDistinctIntegers(self, nums):
-        def reverse(nums):
-            rev=0
-            while nums>0:
-                rev=rev*10+nums%10
-                nums//=10
-            return rev
-        s= set(nums)
-        for num in nums:
-            s.add(reverse(num))
-        return len(s)            
+        def remove_duplicates(nums):
+            i=0
+            for j in range(1,len(nums)):
+                if nums[j]!=nums[i]:
+                    i+=1
+                    nums[i]=nums[j]
+            return i+1     
+        rev_num=[int(str(x)[::-1]) for x in nums]
+        allnums=nums+rev_num
+        allnums.sort()
+        length=remove_duplicates(allnums)
+        return length
+        
+        
         
