e3ca191a2242465595b1b8da5cf63481bc104f25
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-09-23 00:43:39 +0530

    Time: 16 ms (5.67%) | Memory: 12.5 MB (53.13%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution(object):
+    def maxFrequencyElements(self, nums):
+        freq = Counter(nums)
+        max_freq = max(freq.values())
+        return sum(count for count in freq.values() if count == max_freq)
+         
+        
