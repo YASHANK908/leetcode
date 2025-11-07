e6f789545803998760bef345dab1fa7cf4eee6c2
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-11-07 10:48:42 +0530

    Time: 117 ms (64.29%) | Memory: 18.2 MB (23.29%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,17 +1,20 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
         count={}
+        maxlen=0
         left=0
         maxcount=0
-        maxlen=0
+
         for right in range(len(s)):
             c=s[right]
             count[c]=count.get(c,0)+1
             maxcount=max(maxcount,count[c])
-            while (right-left+1) - maxcount>k:
+
+            while (right-left+1)-maxcount>k:
                 count[s[left]]-=1
                 left+=1
             maxlen=max(maxlen,right-left+1)
-        return maxlen        
-       
-       
+        return maxlen
+
+
+        
