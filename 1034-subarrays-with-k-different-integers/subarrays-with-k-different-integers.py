b133c751a341c85f236f2464fb6dd707796053ce
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-11-11 08:38:21 +0530

    Time: 129 ms (62.49%) | Memory: 19.9 MB (24.21%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,22 +1,19 @@
-from collections import defaultdict
-class Solution(object):
-    def subarraysWithKDistinct(self, nums, k):
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         def atmost(k):
-            count=defaultdict(int)
+            freq={}
             left=0
-            res=0
-            n= len(nums)
-            for right in range(n):
-                if count[nums[right]]==0:
-                    k-=1
-                count[nums[right]]+=1
-                while k<0:
-                    count[nums[left]]-=1
-                    if count[nums[left]]==0:
-                        k+=1
+            count=0
+            for right in range(len(nums)):
+                freq[nums[right]]=freq.get(nums[right],0)+1
+                while len(freq)>k:
+                    freq[nums[left]]-=1
+                    if freq[nums[left]]==0:
+                        del freq[nums[left]]
                     left+=1
-                res+=right-left+1
-            return res
-        return atmost(k)-atmost(k-1)                
+                count+=(right-left+1) 
+            return count 
+        return atmost(k)-atmost(k-1)          
+        
 
         
