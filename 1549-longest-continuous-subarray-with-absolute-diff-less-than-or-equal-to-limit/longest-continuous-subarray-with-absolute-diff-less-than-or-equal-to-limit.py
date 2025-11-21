d570f57a0e5e54c8b8c156e1490318b60543ea39
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-11-21 19:07:53 +0530

    Time: 206 ms (53.44%) | Memory: 26.9 MB (49.05%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,28 +1,30 @@
 from collections import deque
-class Solution(object):
-    def longestSubarray(self, nums, limit):
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        maxdeq=deque()
+        mindeq=deque()
+        ans=0
         left=0
-        res=0
-        maxd=deque()
-        mind=deque()
 
-        for right,num in enumerate(nums):
-            while maxd and nums[maxd[-1]]<num:
-                maxd.pop()
-            maxd.append(right)
+        for right in range(len(nums)):
+            while maxdeq and nums[right]>nums[maxdeq[-1]]:
+                maxdeq.pop()
+            maxdeq.append(right)
 
-            while mind and nums[mind[-1]]>num:
-                mind.pop() 
-            mind.append(right)     
+            while mindeq and nums[mindeq[-1]]>nums[right]:
+                mindeq.pop()
+            mindeq.append(right)
 
-            while nums[maxd[0]]-nums[mind[0]]>limit:
+            while nums[maxdeq[0]]-nums[mindeq[0]]>limit:
+                if maxdeq[0]==left:
+                    maxdeq.popleft()
+                if mindeq[0]==left:
+                    mindeq.popleft()
                 left+=1
-                if maxd[0]<left:
-                    maxd.popleft()
-                if mind[0]<left:
-                    mind.popleft()
-            res =max(res,right-left+1)
-        return res      
+            ans=max(ans,right-left+1)
+        return ans
+            
 
 
+        
         
