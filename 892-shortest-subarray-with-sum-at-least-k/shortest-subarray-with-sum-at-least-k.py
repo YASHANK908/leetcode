fb523e27c1d2ed821b5d3c51716fc3be46d68fc1
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-11-23 14:25:54 +0530

    Time: 224 ms (38.83%) | Memory: 34 MB (41.75%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -1,19 +1,22 @@
 from collections import deque
-class Solution(object):
-    def shortestSubarray(self, nums, k):
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        
         n=len(nums)
-        prefix=[0]*(n+1)
-        for i in range(n):
-            prefix[i+1]=prefix[i]+nums[i]
-        dq=deque()
+        prefix=0
+        prefix_sum=[0]*(n+1)
+        dq=deque([0])
         res=float('inf')
-        for j in range(n+1):
-            while dq and prefix[j]-prefix[dq[0]]>=k:
-                res=min(res,j-dq.popleft())
-            while dq and prefix[j]<=prefix[dq[-1]]:
+
+        for j in range(n):
+            prefix+=nums[j]
+            prefix_sum[j+1]=prefix
+
+            while dq and prefix_sum[j+1]-prefix_sum[dq[0]]>=k:
+                res=min(res,(j+1)-dq[0])
+                dq.popleft()
+            while dq and prefix_sum[j+1]<=prefix_sum[dq[-1]]:
                 dq.pop()
-            dq.append(j) 
-        return res if res!=float('inf') else -1                
-        
-        
+            dq.append(j+1)
+        return res if res!=float('inf') else -1
         
