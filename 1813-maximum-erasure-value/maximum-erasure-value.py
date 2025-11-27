9d85853f5522ea13837bd88deee25f3c6986eae1
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-11-27 14:59:21 +0530

    Time: 258 ms (37.58%) | Memory: 29 MB (30.28%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,14 +1,12 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         freq={}
-        left=0
         Sum=0
         maxsum=0
-
+        left=0
         for right in range(len(nums)):
-            
             while nums[right] in freq:
-                Sum-=nums[left]
+                Sum-=nums[left] 
                 freq[nums[left]]-=1
                 if freq[nums[left]]==0:
                     del freq[nums[left]]
@@ -17,5 +15,6 @@ class Solution:
             Sum+=nums[right]
             maxsum=max(maxsum,Sum)
         return maxsum
-
+            
+            
         
