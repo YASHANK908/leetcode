e128aa4cac0837e81f3c24f81461da6fd9ad4c41
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-12-11 19:43:37 +0530

    Time: 99 ms (36.53%) | Memory: 28.6 MB (69.91%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,45 +1,10 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        def heapify_up(heap,index):
-            parent=(index-1)//2
-            while index>0 and heap[index]<heap[parent]:
-                heap[index],heap[parent]=heap[parent],heap[index]
-                index=parent
-                parent=(index-1)//2
-        def heapify_down(heap,index,n):
-            
-            while True:
-                left= 2*index+1
-                right=2*index+2
-                smallest=index
-
-                if left<n and heap[left]<heap[smallest]:
-                    smallest=left
-                if right<n and heap[right]<heap[smallest]:
-                    smallest=right
-                if smallest==index:
-                    break
-                heap[index],heap[smallest]=heap[smallest],heap[index]
-                index=smallest
-
-        def push(heap,val):
-            heap.append(val)
-            heapify_up(heap,len(heap)-1)
-        def pop(heap):
-            if len(heap)==1:
-                return heap.pop()
-            root=heap[0]
-            heap[0]=heap.pop()
-            heapify_down(heap,0,len(heap))
-            return root
-        heap=[]      
-        for num in nums:
-            push(heap,num)
-            if len(heap)>k:
-                pop(heap)
-        return heap[0]        
-
-
-        
-        
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minheap=[]
+        for x in nums:
+            heapq.heappush(minheap,x)
+            if len(minheap)>k:
+                heapq.heappop(minheap)
+        return minheap[0]
         
