114e9b5abc4826ef7db7f721d5d5700c38df35f1
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-12-14 16:38:59 +0530

    Time: 15 ms (88.94%) | Memory: 18.3 MB (65.23%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,30 +1,26 @@
-from collections import deque
-class Solution(object):
-    def maxAreaOfIsland(self, grid):
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows,cols=len(grid),len(grid[0])
-        def bfs(r,c):
-            q=deque()
-            q.append((r,c))
 
+        def dfs(r,c):
+
+            if r<0 or r>=rows or c<0 or c>=cols:
+                return 0
+            if grid[r][c]==0:
+                return 0
             grid[r][c]=0
             area=1
-
-            while q:
-                row,col=q.popleft()
-                for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
-                    nr,nc=row+dr,col+dc
-                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
-                        grid[nr][nc]="0"
-                        q.append((nr,nc))
-                        area+=1
+            area+=dfs(r+1,c)
+            area+=dfs(r-1,c)
+            area+=dfs(r,c+1)
+            area+=dfs(r,c-1)
+            
             return area
+        
         maxarea=0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]==1:
-                    maxarea=max(maxarea,bfs(r,c))
-        return maxarea             
-
-
-
+                    maxarea=max(maxarea,dfs(r,c))
+        return maxarea
         
