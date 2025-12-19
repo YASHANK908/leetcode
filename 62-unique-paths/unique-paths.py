e0d387d3279178ad940f85619107b54c93015140
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-12-19 11:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (99.98%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,9 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m==0 or n==0:
-            return 0
-        if m==1 or n==1:
-            return 1
+         
         
         dp=[[0]*n for _ in range(m)]
 
