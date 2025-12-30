384a7491cb9855bb9fd412279fac42ff0b534ac6
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2025-12-30 10:16:17 +0530

    Time: 1 ms (42.03%) | Memory: 17.9 MB (95.48%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -1,36 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseKGroup(self, head, k):
-        def reverse(start,end):
-            prev=None
-            current=start
-            while current!=end:
-                next=current.next
-                current.next=prev
-                prev=current
-                current=next
-            return prev   
-        temp=head
-        l=0
-        while temp:
-            l+=1
-            temp=temp.next
-        dummy=ListNode()
-        dummy.next=head
-        prevg=dummy
-        while l>=k:
-            start=prevg.next
-            end=start
-            for _ in range(k):
-                end=end.next
-            newhead=reverse(start,end)
-            prevg.next=newhead
-            start.next=end
-            prevg=start
-            l-=k
-        return dummy.next    
-         
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        node=head
+        count=0
+        while node and count<k:
+            node=node.next
+            count+=1
+        if count<k:
+            return head
+        
+        prev=None
+        curr=head
+        for _ in range(k):
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+        head.next=self.reverseKGroup(curr,k)
+        return prev
+
+        
