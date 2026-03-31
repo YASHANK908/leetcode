a29e0efd530d6fea85afd341f6b060db1e8993ba
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2026-03-31 08:49:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (98.55%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -3,7 +3,7 @@ class Solution:
         left,right=0,len(nums)-1
 
         while left<=right:
-            mid=left+(right-left)//2
+            mid= (left+right)//2
             if nums[mid]==target:
                 return mid
             elif nums[mid]<target:
@@ -11,4 +11,5 @@ class Solution:
             else:
                 right=mid-1
         return left
+            
         
