d6a50466d551d304447af73ba6b1d9b92f2d7620
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2026-04-08 14:54:51 +0530

    Time: 1 ms (89.61%) | Memory: 22.2 MB (93.87%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,10 +1,11 @@
 from collections import deque
-class Solution(object):
-    def solve(self, board):
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
         if not board or not board[0]:
             return
         rows,cols=len(board),len(board[0])
         q=deque()
+
         for r in range(rows):
             for c in [0,cols-1]:
                 if board[r][c]=="O":
@@ -13,20 +14,26 @@ class Solution(object):
             for r in [0,rows-1]:
                 if board[r][c]=="O":
                     q.append((r,c))
+            
         while q:
-            r,c=q.popleft()
-            if r<0 or c<0 or r>=rows or c>=cols or board[r][c]!="O":
+            row,col=q.popleft()
+            if row<0 or col<0 or row>=rows or col>=cols or board[row][col]!="O":
                 continue
-            board[r][c]="#"
+            board[row][col]="#"
             for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
-                q.append((r+dr,c+dc))  
+                q.append((row+dr,col+dc))
+            
         for r in range(rows):
             for c in range(cols):
-                if board[r][c]=="O":
+                if board[r][c]=="#":
+                    board[r][c]="O"
+                elif board[r][c]=="O":
                     board[r][c]="X"
-                elif board[r][c]=="#":
-                    board[r][c]="O"    
+         
 
+                
+                
 
-
+        
+        
         
