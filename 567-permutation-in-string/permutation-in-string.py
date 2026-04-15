33dcc6d664924bc8a9244fbb42a3a121ee160479
YASHANK908 <101728365+YASHANK908@users.noreply.github.com>
2026-04-15 20:54:25 +0530

    Time: 24 ms (43.16%) | Memory: 19.3 MB (89.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,17 +2,21 @@ from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         need=Counter(s1)
-        window=Counter()
-        req_len=len(s1)
+        window={}
+        req=len(s1)
         left=0
         for right in range(len(s2)):
-            window[s2[right]]+=1
-            while right-left+1>req_len:
+            c=s2[right]
+            window[c]=window.get(c,0)+1
+
+            while right-left+1>req:
                 window[s2[left]]-=1
                 if window[s2[left]]==0:
                     del window[s2[left]]
                 left+=1
-            if right-left+1==req_len and window==need:
+            if right-left+1==req and window==need:
                 return True
         return False
+   
+   
         
